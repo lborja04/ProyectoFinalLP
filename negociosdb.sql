2eb737ed28ccf1f6e88b03d7e1e622c0bbd54e35
lborja04 <lborjaze@fiec.espol.edu.ec>
2025-01-30 00:06:37 -0500

    actualizacion del backend

--- negociosdb.sql
+++ negociosdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-01-2025 a las 05:15:35
+-- Tiempo de generación: 30-01-2025 a las 05:48:55
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -43,19 +43,9 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `nombre`, `categoria`, `descripcion`, `precio`, `imagen`, `estado`, `vendedor_id`) VALUES
-(2, 'Camiseta', 'Ropa', 'Camiseta de algodón', 20, '0', 'reservado', 21),
-(3, 'Camiseta', 'Ropa', 'Camiseta de algodón', 20, '0', 'activo', 21),
-(4, 'Camiseta', 'Ropa', 'Camiseta de algodón', 20, '0', 'activo', 21),
-(5, 'Camiseta', 'Ropa', 'Camiseta de algodón', 20, '0', 'activo', 21),
-(6, 'Camiseta', 'Ropa', 'Camiseta de algodón', 20, '0', 'activo', 21),
-(7, 'Pantalón Moderno', 'Ropa', 'Pantalón moderno de mezclilla', 40, 'imagen_moderno.jpg', 'inactivo', 29),
-(8, 'Zapatos', 'Calzado', 'Zapatos deportivos', 50, '0', 'inactivo', 33),
-(9, 'Zapatos', 'Calzado', 'Zapatos deportivos', 50, '0', 'inactivo', 46),
-(10, 'Zapatos', 'Calzado', 'Zapatos deportivos', 50, '0', 'inactivo', 49),
-(11, 'Zapatos', 'Calzado', 'Zapatos deportivos', 50, '0', 'reservado', 50),
-(12, 'Mochila', 'Accesorios', 'Mochila de viaje', 80, '0', 'reservado', 51),
-(13, 'Mochila', 'Accesorios', 'Mochila de viaje', 80, '0', 'reservado', 54),
-(14, 'Laptop Gamer', 'Electrónica', 'Laptop para juegos de última generación', 1300, 'imagen_gamer.jpg', 'inactivo', 63);
+(17, 'Camiseta del RayoMcQueen', 'Camiseta', 'La mejor camiseta de Cars, sientente como un rayo veloz con esta prenda.', 25, '679af24536d11_camisa-mcqueen.jpg', 'activo', 64),
+(18, 'Pantalon de Frozen', 'Pantalon', 'Pantalon perfecto para tus suenos de ser una princesa Disney', 35, '679af7c04be44_pantalon-frozen.jpg', 'inactivo', 64),
+(19, 'Zapatos Buzz Lightyear', 'Zapatos', 'Ve al infinito y mas alla con estos zapatos', 41, '679b02e97958a_zapatos-buzz.jpg', 'activo', 64);
 
 -- --------------------------------------------------------
 
@@ -75,15 +65,7 @@ CREATE TABLE `transacciones` (
 --
 
 INSERT INTO `transacciones` (`id`, `producto_id`, `comprador_id`, `fecha`) VALUES
-(2, 2, 21, '2025-01-17 22:48:45'),
-(3, 7, 29, '2025-01-17 23:18:09'),
-(4, 8, 33, '2025-01-17 23:21:51'),
-(5, 9, 46, '2025-01-18 02:48:11'),
-(6, 10, 49, '2025-01-18 02:52:11'),
-(7, 11, 50, '2025-01-18 03:48:34'),
-(8, 12, 51, '2025-01-18 03:51:13'),
-(9, 13, 54, '2025-01-18 03:51:30'),
-(10, 14, 63, '2025-01-18 04:06:11');
+(11, 18, 64, '2025-01-30 04:08:51');
 
 -- --------------------------------------------------------
 
@@ -104,18 +86,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `email`, `clave`, `rol`) VALUES
-(21, 'Juan Pérez', 'juan@example.com', '$2y$10$G9wnH8GNbmxmA1YYGCSq7efx0n92qTW3.ji5idQIUBh8ZhgIVy4/G', 'comprador'),
-(29, 'María Actualizada', 'maria@actualizada.com', '$2y$10$L20Lh2Si0GKkCtbRwsCTReaC6jWifDS4ibtI.GaFcjKj/0.LowRgS', 'vendedor'),
-(30, 'María López', 'maria@example.com', '$2y$10$gLMizhbYoi19lPJgndS3Xewr/OBIQvEUN2NVuBjbt8NgW.VNnO/wK', 'comprador'),
-(33, 'Carlos Actualizado', 'carlos@actualizado.com', '$2y$10$9HmN4Jx5yKjWHTWBIxZpd.f0PjPWg3y8kOWmcDv9z/0mQRACDT5p2', 'vendedor'),
-(34, 'Carlos Méndez', 'carlos@example.com', '$2y$10$YBfYXR54EjTYVSH8wGO9R.iAOK95G8pxr0tSY9LdtfkUUsL9BHFpm', 'comprador'),
-(42, 'Carlos Méndez', 'carlos@espol.com', '$2y$10$MFnTv/f4f1gcKNdfvnrDvujWLH4Tv8tAG9I4wRL/vSrylr7lBLf7e', 'comprador'),
-(46, 'Carlos Méndez', 'carlos@uess.com', '$2y$10$QW/OMGDu.BaYOoaRTlw5G.zI5.lALLy5QDfMIiXE4mC1dhZURlOg2', 'comprador'),
-(49, 'Carlos Actualizado', 'carlos@ups.com', '$2y$10$pQiOpow3i1nkuTLLNxgqd.6H3pidnamrbIR7Jxpf9P3dQyvTUKci.', 'vendedor'),
-(50, 'Carlos Actualizado', 'carlos@harvard.com', '$2y$10$p6M4vcQNmyKxMvZ1/bEje.5rLDXHDzEcMpcVU5msG76Sw9g7xY2va', 'vendedor'),
-(51, 'María Actualizada', 'maria@stanford.edu', '$2y$10$WP1R0TbedLR3SgV/TzfGzuHpRMCGoFLmbN30KFP2lTpe9FdEGoGHm', 'vendedor'),
-(54, 'María González', 'maria@harvard.edu', '$2y$10$WBF7rTRl0YYaPs2b24HqwuZVRb6OvMPK7LEsCeVW38l6lGPwUqIFC', 'comprador'),
-(63, 'Ana Actualizada', 'ana@example.com', '$2y$10$1X4DgUjRSTlARpA62oR9xOUdkmuWzRocObmO3eguqHDZQI91h6hnq', 'vendedor');
+(64, 'Jesus Suarez', 'jessua@espol.edu.ec', '$2y$10$50AXPVZwPeGYQQecljzLyuWvuXQ4osOEmJDKLFq2rectOhf49Biju', 'vendedor'),
+(66, 'Luis Borja', 'laborja@espol.edu.ec', '$2y$10$3INGDeb50Cs1K4GzwoI/6.ZLDB9ud103qTGotNLUJq7in718Hsoly', 'vendedor'),
+(67, 'Alejandro Diez', 'alediez@espol.edu.ec', '$2y$10$mYvkgsqwx66XK5gi6jGl1O.Ruc23JC.Ru3Uhtg3H5cqZyEmlHjNqW', 'vendedor');
 
 --
 -- Índices para tablas volcadas
@@ -151,19 +124,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- Restricciones para tablas volcadas
